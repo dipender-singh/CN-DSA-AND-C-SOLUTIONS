ef1d78b4f8abe4576d9f4f5425b06f6b60bcc680
Dipender Singh <56503546+dipender-singh@users.noreply.github.com>
2021-12-16 21:25:36 +0530

    Add files via upload

---
+++ OOPS 3/POLYMORPHISM COMPILE TIME/Car.cpp
@@ -0,0 +1,11 @@
+class Car : public Vehicle {
+	public :
+		int numGears;
+
+
+		void print() {
+			cout << "Car" << endl;
+		}
+
+
+};
---
+++ OOPS 3/POLYMORPHISM COMPILE TIME/Vehicle.cpp
@@ -0,0 +1,11 @@
+class Vehicle {
+	public :
+		string color;
+
+	void print() {
+		cout << "Vehicle" << endl;
+	}
+
+
+};
+
